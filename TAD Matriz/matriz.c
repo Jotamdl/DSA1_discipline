6ed529cfb36cdcde360b8aad91d7bf229402352c
João Hugo Martins da Luz <joaohugomartinsdaluz@gmail.com>
2025-02-25 23:48:56 -0300

    finished TAD Matriz ex fst try

--- TAD Matriz/matriz.c
+++ TAD Matriz/matriz.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "matriz.h"
+#define MAX(x,y) ((x) > (y) ? (x) : (y))
 
 struct Matriz {
     double** array;
@@ -62,10 +63,91 @@ bool matriz_equals(Matriz* matriz1, Matriz* matriz2) {
     for (int i = 0; i < matriz1->rows; i++) {
         for (int j = 0; j < matriz1->cols; j++) {
             if (matriz1->array[i][j] != matriz2->array[i][j]) {
+                printf("\n%f != %f\n",matriz1->array[i][j],matriz2->array[i][j]);
                 return false;
             }
         }
     }
 
     return true;
+
+}
+
+Matriz* matriz_add(Matriz* matriz1, Matriz* matriz2) {
+    int rows = MAX(matriz1->rows, matriz2->rows);
+    int cols = MAX(matriz1->cols, matriz2->cols);
+    Matriz* matriz3 = matriz_create(rows, cols);
+
+    double cell1, cell2;
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            cell1 = (matriz1->rows <= r || matriz1->cols <= c) ? 0 : matriz1->array[r][c];
+            cell2 = (matriz2->rows <= r || matriz2->cols <= c) ? 0 : matriz2->array[r][c];
+
+            matriz3->array[r][c] = cell1 + cell2;
+        }
+    }
+
+    return matriz3;
+}
+
+Matriz* matriz_sub(Matriz* matriz1, Matriz* matriz2) {
+    int rows = MAX(matriz1->rows, matriz2->rows);
+    int cols = MAX(matriz1->cols, matriz2->cols);
+    Matriz* matriz3 = matriz_create(rows, cols);
+
+    double cell1, cell2;
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            cell1 = (matriz1->rows <= r || matriz1->cols <= c) ? 0 : matriz1->array[r][c];
+            cell2 = (matriz2->rows <= r || matriz2->cols <= c) ? 0 : matriz2->array[r][c];
+
+            matriz3->array[r][c] = cell1 - cell2;
+        }
+    }
+
+    return matriz3;
+}
+
+Matriz* matriz_scalar_mul(Matriz* matriz, double scalar) {
+    int rows = matriz->rows;
+    int cols = matriz->cols;
+    Matriz* matriz3 = matriz_create(rows, cols);
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            matriz3->array[r][c] = matriz->array[r][c] * scalar;
+        }
+    }
+
+    return matriz3;
+}
+
+Matriz* matriz_transpose(Matriz* matriz) {
+    int rows = matriz->rows;
+    int cols = matriz->cols;
+    Matriz* matriz3 = matriz_create(cols, rows);
+
+    for (int r = 0; r < rows; r++)
+    {
+        for (int c = 0; c < cols; c++)
+        {
+            matriz3->array[c][r] = matriz->array[r][c];
+        }
+    }
+
+    return matriz3;
+}
+
+
+bool matriz_is_symmetric(Matriz* matriz) {
+    Matriz* matriz3 = matriz_transpose(matriz);
+
+    return matriz_equals(matriz, matriz3);
 }
