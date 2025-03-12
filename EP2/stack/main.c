71803df6e2eadf7b2bd2a16256fcf7d044a7e6ac
João Hugo Martins da Luz <joaohugomartinsdaluz@gmail.com>
2025-03-11 23:00:29 -0300

    completing stack exercise

--- EP2/stack/main.c
+++ EP2/stack/main.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
-#include <pilha.h>
+#include "pilha.h"
  
 /*Protótipo da função */
-int parenthesis_balance (char* s);  
+int parenthesis_balance (char* s) {
+    int i = 0;
+    char t = s[0];
+    Pilha* p = pilha_cria();
+    while (t != '\0') {   
+        if (t == '(') {
+            pilha_push(p, 1);
+        } else if (t == ')') {
+            if (pilha_vazia(p)) {
+                printf("incorrect\n");
+                return 0;
+            };
+            pilha_pop(p);
+        }
+        
+        t = s[++i];
+    }
+    
+    if (!pilha_vazia(p)) {
+        printf("incorrect\n");
+        return 0;
+    } else {
+        printf("correct\n");
+        return 1;
+    }
+    
+};  
 
 int main() {
-    // Sua solução
+
     return 0;
 }
